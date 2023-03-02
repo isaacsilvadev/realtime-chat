464937dacdb3180b902a6e8ea633ad5da93b6dd6
isaacsilva-cit <isaacsilva@ciandt.com>
2023-03-02 01:38:17 -0300

    remove comments

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 import { createServer } from 'http';
 import staticHandler from 'serve-handler';
 import ws, { WebSocketServer } from 'ws';
-//serve static folder
-const server = createServer((req, res) => {   // (1)
+
+const server = createServer((req, res) => {
     return staticHandler(req, res, { public: 'public' })
 });
-const wss = new WebSocketServer({ server }) // (2)
+const wss = new WebSocketServer({ server })
 wss.on('connection', (client) => {
     console.log('Client connected !')
-    client.on('message', (msg) => {    // (3)
+    client.on('message', (msg) => {
         console.log(`Message:${msg}`);
         broadcast(msg)
     })
 })
-function broadcast(msg) {       // (4)
+function broadcast(msg) {
     for (const client of wss.clients) {
         if (client.readyState === ws.OPEN) {
             client.send(msg)
