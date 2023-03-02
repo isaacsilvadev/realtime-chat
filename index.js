d2b8dfbf96305e6efc6992be39f3283652ded6de
isaacsilva-cit <isaacsilva@ciandt.com>
2023-03-02 01:41:12 -0300

    format code

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
-import { createServer } from 'http';
-import staticHandler from 'serve-handler';
-import ws, { WebSocketServer } from 'ws';
+import { createServer } from "http";
+import staticHandler from "serve-handler";
+import ws, { WebSocketServer } from "ws";
 
 const server = createServer((req, res) => {
-    return staticHandler(req, res, { public: 'public' })
+  return staticHandler(req, res, { public: "public" });
 });
-const wss = new WebSocketServer({ server })
-wss.on('connection', (client) => {
-    console.log('Client connected !')
-    client.on('message', (msg) => {
-        console.log(`Message:${msg}`);
-        broadcast(msg)
-    })
-})
+const wss = new WebSocketServer({ server });
+wss.on("connection", (client) => {
+  console.log("Client connected !");
+  client.on("message", (msg) => {
+    console.log(`Message:${msg}`);
+    broadcast(msg);
+  });
+});
 function broadcast(msg) {
-    for (const client of wss.clients) {
-        if (client.readyState === ws.OPEN) {
-            client.send(msg)
-        }
+  for (const client of wss.clients) {
+    if (client.readyState === ws.OPEN) {
+      client.send(msg);
     }
+  }
 }
 server.listen(process.argv[2] || 8080, () => {
-    console.log(`server listening...`);
-})
+  console.log(`server listening...`);
+});
